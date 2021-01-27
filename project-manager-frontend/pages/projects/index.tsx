ccf49bd3a049188958a7959f4ef4d0afe90b0787
shreyashc <shreyashc018@gmail.com>
2021-01-27 23:03:41 +0530

    load projects

--- project-manager-frontend/pages/projects/index.tsx
+++ project-manager-frontend/pages/projects/index.tsx
@@ -1,31 +1,40 @@
+import { Spinner } from "../../components/Spinner";
 import { useMyProjectsQuery } from "../../generated/graphql";
 import withApollo from "../../utils/withApollo";
 
 const projects = () => {
   const { data, loading } = useMyProjectsQuery();
-  if (loading) return null;
+
+  if (!loading && !data) {
+    return (
+      <div>
+        <div>Something Went Wrong</div>
+      </div>
+    );
+  }
   return (
-    <div className="md:container md:mx-auto p-4">
+    <div className="container">
       <div className="max-w-4xl mx-auto">
         <div className="flex items-center bg-blue-400 px-4 py-1 rounded">
           <h2 className="my-6 font-semibold text-xl">My Projects</h2>
-          <a
-            className="mx-4 ml-auto text-gray-500 font-semibold bg-gray-100 py-1 px-2 rounded"
-            href="add.html "
-          >
+          <a className="mx-4 ml-auto text-gray-500 font-semibold bg-gray-100 py-1 px-2 rounded">
             Add Project
           </a>
         </div>
-        <div className="projects">
-          {data.myProjects.map((project) => (
-            <div className="project p-3 bg-gray-600 my-3" key={project.id}>
-              <a>
-                <h3 className="text-xl">{project.title}</h3>
-                <p className="mt-2 text-gray-300">{project.description}</p>
-              </a>
-            </div>
-          ))}
-        </div>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <div className="projects">
+            {data.myProjects.map((project) => (
+              <div className="project p-3 bg-gray-600 my-3" key={project.id}>
+                <a>
+                  <h3 className="text-xl">{project.title}</h3>
+                  <p className="mt-2 text-gray-300">{project.description}</p>
+                </a>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
